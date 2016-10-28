6e9fa0f3be0daf8e2b3fcc014af95517758b124b
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-10-28 21:19:10 +0700

    node modules

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 'use strict'
 
+// ---------------------------------------------------------------------
+// NODE Modules
+// ---------------------------------------------------------------------
+
+// Express Dependencies
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+// Initiate Express
+const app = express()
+const router = app.Router()
+
+// ---------------------------------------------------------------------
+// APP CONFIGURATION
+// ---------------------------------------------------------------------
