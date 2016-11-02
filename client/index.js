54fc9e29744a5a7c511bfc83777ff5baab660f89
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-02 15:19:19 +0700

    added description in index.js

--- client/index.js
+++ client/index.js
@@ -1,3 +1,11 @@
 $('document').ready(() => {
-  $('#description').html('change this paragraph')
+  let $description = $('#description')
+
+  // $description.html('change this paragraph')
+
+  $.getJSON(`http://localhost:3000/books`, (data) => {
+    console.log(data)
+    $description.html(`${data[0].id}: ${data[0].name} - ${data[0].price}$`)
+  })
+
 })
