fb6190cc3a271b277233532390a5081e72f825e2
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-02 17:07:26 +0700

    added models books.js

---
+++ server/models/books.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const Book = new Schema({
+  isbn  : String,
+  name  : String,
+  price : Number
+}, {
+  timestamp: true
+})
+
+module.exports = mongoose.model('books', Book)
