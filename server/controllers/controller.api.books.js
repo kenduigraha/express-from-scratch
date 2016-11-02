96fc23e33e64dbd884ab73ba5008132ae059c2a2
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-02 15:41:49 +0700

    updated routes & controller books

--- server/controllers/controller.api.books.js
+++ server/controllers/controller.api.books.js
@@ -1,7 +1,11 @@
 //data books
 const books = require('../data/books')
 
-let viewBooks = (req, res, next) => {
+/*
+  GET
+  /api/books
+*/
+let getBooks = (req, res, next) => {
   //req.body.body = /
   //req.params = /params
   //req.query = ?query={value}
@@ -14,7 +18,77 @@ let viewBooks = (req, res, next) => {
   // books.push(new_book)
 }
 
+let ping = (req, res, next) => {
+  res.send('PONG!')
+}
+
+/*
+  GET
+  /api/books/:id
+*/
+let getBookById = (req, res, next) => {
+  let book = books.filter( (book) => {
+    return book.id == req.params.id
+  })[0]
+
+  if(!book) res.status(404).json({message: "Data Not Found"})
+
+  res.json(book)
+}
+
+/*
+  PUT
+  /api/books/:id
+*/
+let updateBookById = (req, res, next) => {
+  // Get book by id
+  const book = books.filter((book) => {
+    return book.id === Number(req.params.id)
+  })[0]
+
+  // Send 404 error if book not found
+  if(!book) res.status(404).json({'message': "No Book Found"})
+
+  const index = books.indexOf(book)
+  const key = Object.keys(req.body)
+
+  key.forEach(key => {
+    // book[key] = req.body[key]
+    book.id = Number(req.body.id),
+    book.name = req.body.name,
+    book.price = Number(req.body.price)
+  })
+
+  books[index] = book
+
+  // Send success message
+  res.json({ 'message' : `Book ${req.params.id} has been updated`})
+}
+
+/*
+  DELETE
+  /api/books/:id
+*/
+let deleteBookById = (req, res, next) => {
+  // Get book by id
+  const book = books.filter((book) => {
+    return book.id === Number(req.params.id)
+  })[0]
+
+  // Send 404 error if book not found
+  if(!book) res.status(404).json({'message': "No Book Found"})
+
+  // Delete the book by id
+  books.splice(books.indexOf(book), 1)
+
+  // Send success message
+  res.json({ 'message' : `Book ${req.params.id} has been deleted`})
+}
 
 module.exports = {
-  viewBooks: viewBooks
+  getBooks: getBooks,
+  ping: ping,
+  getBookById: getBookById,
+  updateBookById: updateBookById,
+  deleteBookById: deleteBookById
 }
--- server/routes/books.js
+++ server/routes/books.js
@@ -13,62 +13,15 @@ const app = express()
 const router = express.Router()
 const books = require('../data/books')
 
-router.get('/ping', (req, res, next) => {
-  res.send('PONG!')
-})
+router.get('/ping', controller.ping)
 
-router.get('/books', controller.viewBooks)
+router.get('/', controller.getBooks)
 
-router.get('/books/:id', (req, res, next) => {
-  let book = books.filter( (book) => {
-    return book.id == req.params.id
-  })[0]
+router.get('/:id', controller.getBookById)
 
-  if(!book) res.status(404).json({message: "Data Not Found"})
+router.put('/:id', controller.updateBookById)
 
-  res.json(book)
-})
+router.delete('/:id', controller.deleteBookById)
 
-// DELETE
-router.delete('/books/:id', (req, res, next) => {
-  // Get book by id
-  const book = books.filter((book) => {
-    return book.id === Number(req.params.id)
-  })[0]
-
-  // Send 404 error if book not found
-  if(!book) res.status(404).json({'message': "No Book Found"})
-
-  // Delete the book by id
-  books.splice(books.indexOf(book), 1)
-
-  // Send success message
-  res.json({ 'message' : `Book ${req.params.id} has been deleted`})
-})
-
-router.put('/books/:id', (req, res, next) => {
-  // Get book by id
-  const book = books.filter((book) => {
-    return book.id === Number(req.params.id)
-  })[0]
-
-  // Send 404 error if book not found
-  if(!book) res.status(404).json({'message': "No Book Found"})
-
-  const index = books.indexOf(book)
-  const key = Object.keys(req.body)
-
-  key.forEach(key => {
-    // book[key] = req.body[key]
-    book.id = Number(req.body.id),
-    book.name = req.body.name,
-    book.price = Number(req.body.price)
-  })
-
-  books[index] = book
-
-  // Send success message
-  res.json({ 'message' : `Book ${req.params.id} has been updated`})
-})
 
 module.exports = router
