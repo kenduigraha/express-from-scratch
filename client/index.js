63fa9e3789212f06420a3042bb98832b111e4086
Ken Duigraha Putra <kenduigraha@kudo.co.id>
2018-10-09 10:02:13 +0700

    Remove unused code.

--- client/index.js
+++ client/index.js
@@ -1,8 +1,6 @@
 $('document').ready(() => {
   let $description = $('#description')
 
-  // $description.html('change this paragraph')
-
   $.getJSON(`http://localhost:3000/books`, (data) => {
     console.log(data)
     $description.html(`${data[0].id}: ${data[0].name} - ${data[0].price}$`)
